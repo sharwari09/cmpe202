d69be2a0a34beb55227650497938ad3437dd71cd
sharwari09 <sharwari.phadnis@sjsu.edu>
2018-09-04 19:04:41 -0700

    Adding changes related to support for: - Machine accepting 2 quarters or more. - Machine accepting dimes, nickels and quarters

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -6,15 +6,13 @@ public class GumballMachine
     public boolean has_quarter;
     public boolean has_nickel;
     public boolean has_dime;
-    public int noOfQuarters;
-    public int valueOfCoins;
+    public int valueOfCoins = 0 ;
 
     public GumballMachine( int size )
     {
         // initialise instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
-        this.valueOfCoins = 0;
     }
     
     public void insertQuarter(int coin)
@@ -51,25 +49,36 @@ public class GumballMachine
         {
             case 1:
                 turnCrankFor25Cents();
+                initValueOfcoins();
                 break;
             case 2:
                 turnCrankFor50Cents();
+                initValueOfcoins();
                 break;
-            
+            case 3:
+                turnCrankForAllCents();
+                initValueOfcoins();
+                break;
         }
+    }
+    
+    public void initValueOfcoins()
+    {
+        valueOfCoins = 0;
     }
     
     public void turnCrankFor25Cents()
     {
+        System.out.format( "In 25 cents machine \n \nValue of coins is: %d\n ", valueOfCoins ) ;
         if ( this.has_quarter )
         {
             if ( this.num_gumballs > 0 )
             {
                 this.num_gumballs-- ;
                 this.has_quarter = false ;
-                this.valueOfCoins = this.valueOfCoins - 25;
+                valueOfCoins = valueOfCoins - 25;
                 System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
-                System.out.format( "Value of coins is: %d\n",this.valueOfCoins ) ;
+                System.out.format( "Value of coins is: %d\n", valueOfCoins ) ;
             }
             else
            {
@@ -84,16 +93,16 @@ public class GumballMachine
     
     public void turnCrankFor50Cents()
     {
-        System.out.format( "Value of coins is: %d\n",this.valueOfCoins ) ;
+        System.out.format( "In 50 cents machine \n \nValue of coins is: %d\n ", valueOfCoins ) ;
         if ( this.has_quarter )
         { 
-            if (this.valueOfCoins >= 50)
+            if (valueOfCoins >= 50)
             {
                 if ( this.num_gumballs > 0 )
                 {
                     this.num_gumballs-- ;
                     this.has_quarter = false ;
-                    this.valueOfCoins = this.valueOfCoins - 50;
+                    valueOfCoins = valueOfCoins - 50;
                     System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
                 }
                 else
@@ -103,14 +112,37 @@ public class GumballMachine
             }
             else
             {
-                System.out.println( "You have inserted " + this.valueOfCoins + "cents" ) ;
-                System.out.format("Please insert more %d to get a gumball\n", (50 - this.valueOfCoins)) ;
+                System.out.println( "You have inserted " + valueOfCoins + "cents" ) ;
+                System.out.format("Please insert more %d to get a gumball\n", (50 - valueOfCoins)) ;
             }
         }
         else 
         {
             System.out.println( "Please insert a quarter" ) ;
         } 
+    }
+    
+    public void turnCrankForAllCents()
+    {
+        System.out.format( "In All cents machine \n \nValue of coins is: %d\n ", valueOfCoins ) ;
+        if (valueOfCoins >= 50)
+            {
+                if ( this.num_gumballs > 0 )
+                {
+                    this.num_gumballs-- ;
+                    valueOfCoins = valueOfCoins - 50;
+                    System.out.println( "Thanks for your money.  Gumball Ejected!" ) ;
+                }
+                else
+                {
+                    System.out.println( "No More Gumballs!  Sorry, can't return your  money." ) ;
+                }
+            }
+            else
+            {
+                System.out.println( "You have inserted " + this.valueOfCoins + "cents" ) ;
+                System.out.format("Please insert more %d to get a gumball\n", (50 - valueOfCoins)) ;
+            }
     }
     
     public boolean is_quarter(int coin)
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -5,12 +5,12 @@ public class Main {
     public static void main(String[] args) {
         GumballMachine gumballMachine = new GumballMachine(5);
 
-        System.out.println("Testing 25 Cents machine");
-
+        System.out.println("Testing 25 Cents machine\n");
+        System.out.println("Adding 30 in first machine instead of a quarter\n");
         gumballMachine.insertQuarter( 30 );
+        
         gumballMachine.turnCrank(1);
-
-        System.out.println("Testing 50 Cents machine");
+        System.out.println("Testing 50 Cents machine\n\n");
 
         gumballMachine.insertQuarter( 25 );
         gumballMachine.turnCrank(2);
@@ -18,5 +18,16 @@ public class Main {
         gumballMachine.insertQuarter( 25 );
         gumballMachine.insertQuarter( 30 );
         gumballMachine.turnCrank(2);
+        
+        System.out.println("Testing All Cents machine\n\n");
+        gumballMachine.insertNickel( 5 );
+        gumballMachine.turnCrank(3);
+        
+        gumballMachine.insertDime( 10 );
+        gumballMachine.insertDime( 10 );
+        gumballMachine.turnCrank(3);
+        gumballMachine.insertQuarter( 25 );
+        gumballMachine.turnCrank(3);
+
     }
 }
